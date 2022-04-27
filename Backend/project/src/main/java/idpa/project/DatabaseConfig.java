953cfaa1c8b9420299052e40ba79491e337eed31
tobo3199 <tobias.sauter.003@outlook.com>
2022-04-27 15:15:15 +0200

    generate random number for pin

--- Backend/project/src/main/java/idpa/project/DatabaseConfig.java
+++ Backend/project/src/main/java/idpa/project/DatabaseConfig.java
@@ -14,6 +14,7 @@ import javax.sql.DataSource;
  * @author: Tobias Sauter
  * @version:
  */
+/*
 @Configuration
 public class DatabaseConfig {
 
@@ -27,3 +28,4 @@ public class DatabaseConfig {
         return new HikariDataSource(config);
     }
 }
+*/
