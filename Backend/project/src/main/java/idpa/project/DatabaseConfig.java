361d674464b98dc58f32f3a1939f93edebab1d83
tobo3199 <tobias.sauter.003@outlook.com>
2022-04-27 13:59:27 +0200

    DatabseConfig

---
+++ Backend/project/src/main/java/idpa/project/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package idpa.project;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Klasse:
+ *
+ * @author: Tobias Sauter
+ * @version:
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
