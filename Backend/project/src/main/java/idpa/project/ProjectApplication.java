bffbf8b7a7dd7b58b7e72e0960652bdbdbceedf1
tobo3199 <tobias.sauter.003@outlook.com>
2022-05-02 12:59:48 +0200

    DatabaseConfig

--- Backend/project/src/main/java/idpa/project/ProjectApplication.java
+++ Backend/project/src/main/java/idpa/project/ProjectApplication.java
@@ -2,21 +2,11 @@ package idpa.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
-	}
-
-	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
 	}
 }
